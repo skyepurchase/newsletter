1d6484766ec68f87835f710572884820e35e388b
Skye Purchase <skyepurchase@gmail.com>
2025-09-15 13:49:01 +0100

    Automatically create folder name

--- create_newsletter.py
+++ create_newsletter.py
@@ -9,29 +9,30 @@ from utils.database import create_newsletter
 if __name__=='__main__':
     parser = ArgumentParser("Create Newsletter")
     parser.add_argument("--title", required=True, help="The title of the newsletter.")
-    parser.add_argument("--folder", required=True, help="The folder to store metadata.")
     parser.add_argument("--email", required=True, help="The email to be used when sending out requests.")
 
     args = parser.parse_args()
     passcode = getpass("Passcode: ")
     pass_hash = hash_passcode(passcode)
 
-    if not os.path.isdir(args.folder):
-        os.makedirs(args.folder)
+    folder = args.title.lower().replace(" ", "_")
+
+    if not os.path.isdir(folder):
+        os.makedirs(folder)
 
     base_config = {
         "name": args.title,
         "email": args.email,
-        "folder": args.folder,
+        "folder": folder,
         "link": "https://skye.purchasethe.uk/projects/newsletter/",
         "issue": 1
     }
 
-    with open(os.path.join(args.folder, "config.yaml")) as f:
+    with open(os.path.join(folder, "config.yaml"), "w") as f:
         yaml.dump(base_config, f)
-    with open(os.path.join(args.folder, "emails.txt")) as f:
+    with open(os.path.join(folder, "emails.txt"), "w") as f:
         pass
 
-    print(f"Update {os.path.join(args.folder, "emails.txt")} to include the emails of the participants.")
+    print(f"Update {os.path.join(folder, 'emails.txt')} to include the emails of the participants.")
 
-    create_newsletter(args.title, pass_hash, args.folder)
+    create_newsletter(args.title, pass_hash, folder)
