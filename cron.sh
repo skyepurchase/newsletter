3a5aee0d8b13b1e79c9b4be96217feae95a7b6a7
Skye Purchase <s.purchase25@imperial.ac.uk>
2026-01-11 17:14:39 +0000

    Fix cron job

--- cron.sh
+++ cron.sh
@@ -4,26 +4,26 @@ week_num=$(date "+%U")
 week_part=$(($week_num % 4))
 
 # Change to point to your newsletter folder
-newsletter_dir="$HOME/public_html/cgi-bin/newsletter"
+mailer="$HOME/public_html/cgi-bin/newsletter/mailer.py"
 
-for newsletter in "$HOME/newsletters/*"; do
+for newsletter in $(find "$HOME/newsletters" -maxdepth 1 -mindepth 1); do
     config="$newsletter/config.yaml"
-    issue="$newsletter/issue"
+    issue= "$newsletter/issue"
 
     if [ $week_part -eq 0 ]; then
         old=$(cat "$issue")
         new=$(expr $old + 1)
         echo $new > "$issue"
-        python3 "$newsletter_dir/mailer.py" -q -c "$config"
+        python3 $mailer -q -c "$config"
         echo "INFO $(date): $newsletter newsletter question request 1"
     elif [ $week_part -eq 1 ]; then
-        python3 "$newsletter_dir/mailer.py" -q -c "$config"
+        python3 $mailer -q -c "$config"
         echo "INFO $(date): $newsletter newsletter question request 2"
     elif [ $week_part -eq 2 ]; then
-        python3 "$newsletter_dir/mailer.py" -a -c "$config"
+        python3 $mailer -a -c "$config"
         echo "INFO $(date): $newsletter newsletter answer request"
     elif [ $week_part -eq 3 ]; then
-        python3 "$newsletter_dir/mailer.py" -c "$config"
+        python3 $mailer -c "$config"
         echo "INFO $(date): $newsletter newsletter published"
     else
         echo "CRITICAL: week part ($week_part) was not in mod 4."
