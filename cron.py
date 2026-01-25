364f45d5d04f60d2aa0ac1f6adbe7adb595ef2bf
Skye Purchase <s.purchase25@imperial.ac.uk>
2026-01-25 20:05:57 +0000

    Switch cronjob to python script
    This way both the cronjob and the renderer can be in agreement on what
    the state of the newsletter is. It would now be possible to switch to
    having the issue in the config YAML again.

---
+++ cron.py
@@ -0,0 +1,74 @@
+#!/bin/python3
+
+
+import json
+import logging
+import mailer
+from utils.constants import State
+from utils.helpers import get_state, check_and_increment_issue, load_config
+from utils.type_hints import MailerConfig
+
+
+if __name__=='__main__':
+    import os
+
+    HOME = os.getenv("HOME")
+    if HOME is None:
+        print("Unable to find $HOME variable")
+        exit(1)
+
+    with open(os.path.join(HOME, ".secrets.json"), "r") as f:
+        SECRETS = json.load(f)
+
+    state = get_state()
+
+    if not os.path.isdir(os.path.join(HOME, "newsletters")):
+        print("Newsletter folder does not exist")
+        exit(2)
+
+    for newsletter in os.listdir(os.path.join(HOME, "newsletters")):
+        success, msg = check_and_increment_issue(newsletter)
+
+        if not success:
+            print(f"Failed to increment the issue for {newsletter}: {msg}")
+            exit(3)
+
+        success, config = load_config(os.path.join(HOME, "newsletters", newsletter), logging.getLogger(__name__))
+
+        if not success:
+            print(f"Unable to load config for {newsletter}")
+            continue
+
+        mail_config = MailerConfig(
+            isQuestion=False,
+            isAnswer=False,
+            isSend=False,
+            isManual=False,
+            password=SECRETS["MAIL_PASS"],
+            debug=False,
+            name=config.name,
+            email=config.email,
+            issue=config.issue,
+            addresses=[config.email],
+            folder=config.folder,
+            link=config.link,
+            text="",
+        )
+
+        msg = ""
+        if state == State.Question:
+            mail_config.isQuestion = True
+            msg = "question request"
+        elif state == State.Answer:
+            mail_config.isAnswer = True
+            msg = "answer request"
+        else:
+            mail_config.isSend = True
+            msg = "publishing"
+
+        mailer.main(mail_config)
+
+        print(f"{newsletter} {msg} successful")
+
+    print("Done")
+    exit(0)
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,16 @@
-import os, yaml, traceback, logging
+import os
+import yaml
+import traceback
+import logging
+from datetime import datetime
 
-from typing import Tuple
+from typing import Optional, Tuple
+
+from .constants import State
 from .type_hints import EmptyConfig, NewsletterConfig
+
+
+NOW = datetime.now()
 
 
 def load_config(
@@ -63,3 +72,58 @@ def load_config(
         issue=issue,
         defaults=config["defaults"]
     )
+
+
+def _get_int_state() -> int:
+    """
+    Return the current week modulo 4
+    """
+    week_num = int(datetime.now().strftime("%U"))
+    return (week_num - 1) % 4 # Change in 2027
+
+
+def get_state() -> State:
+    """
+    Get the current newsletter state based on the current time.
+
+    Returns
+    -------
+    state : State
+        An enum representing the current state
+    """
+    int_state = _get_int_state()
+
+    if int_state <= 1:
+        return State.Question
+    elif int_state == 2:
+        return State.Answer
+    else:
+        return State.Publish
+
+
+def check_and_increment_issue(newsletter_folder: str) -> Tuple[bool, Optional[str]]:
+    """
+    Check whether the issue number should be incremented and increment it if so.
+
+    Returns
+    -------
+    success : bool
+        Whether the incrementing succeeded. True if not incrementation required
+    msg : str | None
+        The error message if required
+    """
+    int_state = _get_int_state()
+
+    if int_state == 0:
+        try:
+            with open(
+                os.path.join("/home/atp45", newsletter_folder, "issue"), "r"
+            ) as issue_file:
+                issue = int(issue_file.read())
+                issue_file.write(str(issue+1))
+        except OSError:
+            return False, "Failed to open issue file"
+        except ValueError:
+            return False, "Failed to parse issue file"
+
+    return True, None
